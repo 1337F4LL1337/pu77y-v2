06fe90184df3bb6d58a49528d3c627fc5ec71ff1
F4LL <61364610+1337F4LL1337@users.noreply.github.com>
2022-09-17 10:53:29 +0200

    Update index.js

--- core/index.js
+++ core/index.js
@@ -1,6 +1,4 @@
 const request = require('request')
-const wait = require('wait')
-const fs = require('fs')
 const pu77y = require('./bin')
 var target = process.argv[2]
 var port = process.argv[3]
@@ -8,37 +6,8 @@ var rek = process.argv[4]
 
 
 process.title = "PU77Y V2 - F4LL [Status = Charging...]"
-let num = [
-"1",
-"2",
-"3",
-"4",
-"5",
-"6",
-"Nucl34r",
-"V.I.P",
-"Power",
-"7",
-"8",
-"9",
-"10",
-"11",
-"12",
-]
-const server = num[Math.floor(Math.random() * num.length)];
-function makeid(length) {
-    var result           = '';
-    var characters       = '123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
-
 
 let sended = 0
-var packett = 0
 console.log(`\n\n                    \x1b[37m|-------------------------------------------|`)
 console.log(`                    |                                           |`)
 console.log(`                    |          [\x1b[32m+\x1b[37m] --> \x1b[36m${target}\x1b[37m                  |`)
